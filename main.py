07a0f15d66cf656cfebed65f2657742b82d6ad21
Ilya Buzuev <ilya.buzuev@mail.ru>
2023-06-30 03:28:44 +0300

    changed the logic of the algorithm

--- main.py
+++ main.py
@@ -1,56 +1,57 @@
-import math
+# Импортирование библиотеки для работы с GCode
+from math import pi
 
-# Задание параметров куба
-cub_side_length = 10.0    # Длина стороны куба в мм
-cub_height = 5.0          # Высота куба в мм
-layer_height = 0.2        # Высота слоя в мм
-feed_rate = 500           # Скорость подачи в мм/мин
+# Размеры куба (в мм)
+length = 10
+width = 10
+height = 10
 
-# Рассчёт количества шагов для двигателей в каждом направлении
-steps_per_mm = 200       # Шагов на мм для 3д принтера
-x_steps = int(cub_side_length * steps_per_mm)
-y_steps = int(cub_side_length * steps_per_mm)
-z_steps = int(cub_height * steps_per_mm)
+# Настройки 3D-принтера
+extruder_temp = 130  # Температура сопла
+bed_temp = 220  # Температура стола
+feedrate = 4600  # Скорость движения подачи материала
 
-# Настройка начальной точки и скорости перемещения
-start_x = 0
-start_y = 0
-start_z = 0
-start_feed_rate = 0
+# Открываем файл для записи GCode
+gcode_file = open("cube.gcode", "w")
 
-# Создание заголовка G-кода (определение начальной точки, метрическая система, скорость и т.д.)
-gcode = "G21 ; Установка в метрическую систему\n"
-gcode += "G90 ; Абсолютные координаты\n"
-gcode += "G1 F%d ; Установка скорости подачи\n" % feed_rate
-gcode += "M107 ; Остановка обдува\n"
-gcode += "G28 X Y Z ; Выход в стартовую точку\n"
+# Функция для записи команды в файл GCode
+def write_command(command):
+    gcode_file.write(command + "\n")
 
-# Создание G-кода для каждого куба
-for z in range(0, z_steps, int(layer_height * steps_per_mm)):
-    end_x = start_x + x_steps
-    end_y = start_y + y_steps
+# Начало программы GCode
+write_command("; Начало программы")
+write_command("G21") # Установка единиц измерения в миллиметрах
+write_command("G90") # Установка абсолютных координат
+# write_command("M109 S" + str(extruder_temp))  # Установка температуры сопла и ждать, пока разогреется
+# write_command("M104 S" + str(bed_temp))  # Установка температуры стола и не ждать, пока разогреется
+# write_command("G28 ; Home all axes")  # Возврат всех осей в начальную позицию
+# write_command("G92 E0 ; Reset extruder")  # Сброс экструдера
+# write_command("G1 F" + str(feedrate))  # Установка скорости подачи материала
+write_command("G0 Z0")
+write_command("G0 X0 Y0")
 
-    # Движение по оси Z
-    gcode += "G1 X%d Y%d Z%d F%d\n" % (start_x, start_y, z / steps_per_mm, feed_rate)
+write_command("\n")
 
-    # Создание контура куба
-    gcode += "G1 X%d Y%d F%d\n" % (end_x, start_y, feed_rate)
-    gcode += "G1 X%d Y%d F%d\n" % (end_x, end_y, feed_rate)
-    gcode += "G1 X%d Y%d F%d\n" % (start_x, end_y, feed_rate)
-    gcode += "G1 X%d Y%d F%d\n" % (start_x, start_y, feed_rate)
-    gcode += "G1 X%d Y%d F%d\n" % (end_x, start_y, feed_rate)
+for z in range(1, height + 1):
+    for x in range(width):
+        write_command("G1 X%d Y%d" % (x, length))
+        write_command("G0 X%d Y%d" % (x + 1, 0))
 
-    # Подъем на следующий слой
-    gcode += "G1 Z%d ; Подъем на следующий слой\n" % ((z + int(layer_height * steps_per_mm)) / steps_per_mm)
+    write_command("G0 X0 Y0 Z%d ; new layer" % (z))
+    # write_command("G0 X0 Y0")
 
-# Завершение работы и сохранение файла G-кода
-gcode += "M107 ; Остановка обдува\n"
-gcode += "G28 X Y Z ; Выход в стартовую точку\n"
-gcode += "M84 ; Остановка двигателей\n"
 
-# Сохранение файла G-кода
-file_name = "cube_%d_%d.gcode" % (cub_side_length, cub_height)
-with open(file_name, "w") as gcode_file:
-    gcode_file.write(gcode)
+write_command("\n")
 
-print("G-код создан и сохранён в файле %s" % file_name)
+# Завершение программы GCode
+write_command("G1 F200 E10")  # Подача материала (конец)
+write_command("M104 S0 ; Отключение обогревателя сопла")
+write_command("M140 S0 ; Отключение обогревателя стола")
+write_command("G92 E0 ; Сброс экструдера")
+write_command("G28 ; Возврат всех осей в начальную позицию")
+write_command("M84 ; Отключение моторов")
+
+# Закрытие файла GCode
+gcode_file.close()
+
+print("Программа GCode для создания куба сгенерирована и сохранена в файле cube.gcode")
